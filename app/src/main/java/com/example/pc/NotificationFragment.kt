87374349ac2d4cd648accbce5005f11ab2c522fb
yamayuka <rinren02rara>
2019-10-26 20:41:41 +0900

    commit

--- app/src/main/java/com/example/pc/NotificationFragment.kt
+++ app/src/main/java/com/example/pc/NotificationFragment.kt
@@ -1,12 +1,12 @@
 package com.example.pc
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class NotificationFragment :Fragment(){
+class NotificationFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_notification, container, false)
         return view
